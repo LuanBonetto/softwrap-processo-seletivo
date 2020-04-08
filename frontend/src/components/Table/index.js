1dc444c28da35bed45a194c0610e53c71315af85
LuanBonetto <luanbonetto42@gmail.com>
2020-04-08 06:30:54 -0300

    add: components, redux

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -1,14 +1,60 @@
-import React from 'react';
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import { Table, Container, Button } from 'react-bootstrap';
-import { FaTrashAlt } from 'react-icons/fa';
+import { FaTrashAlt, FaEdit } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { getPersons, deletePersonByCpf, updatePersonByCpf } from '../../actions/index';
+import ModalUpdatePerson from '../ModalUpdatePerson/index';
 
 export default function ContentTable() {
+  const dispatch = useDispatch();
+  const persons = useSelector((state) => state.allPersons);
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [civilStatus, setCivilStatus] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+
+  useEffect(() => {
+    dispatch(getPersons());
+  }, [dispatch]);
+
+  const onDeletePerson = (cpfPerson) => {
+    dispatch(deletePersonByCpf(cpfPerson));
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = (person) => {
+    setShow(true);
+    setName(person.name);
+    setAge(person.age);
+    setCivilStatus(person.civilStatus);
+    setCpf(person.cpf);
+    setCity(person.city);
+    setState(person.state);
+  };
+
+  const updatePerson = () => {
+    event.preventDefault();
+    const newPerson = {
+      name,
+      age,
+      civilStatus,
+      cpf,
+      city,
+      state,
+    };
+    dispatch(updatePersonByCpf(newPerson));
+  };
+
   return (
     <Container>
       <Table striped bordered hover variant="dark" responsive size="sm">
         <thead>
           <tr>
-            <th>#</th>
             <th>Nome</th>
             <th>Idade</th>
             <th>Estado Civil</th>
@@ -18,20 +64,42 @@ export default function ContentTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Luan</td>
-            <td>20</td>
-            <td>Namorando</td>
-            <td>18337496770</td>
-            <td>Vitória</td>
-            <td>ES</td>
-            <td>
-              <Button variant="danger"><FaTrashAlt /></Button>
-            </td>
-          </tr>
+          { persons.map((person) => (
+            <tr key={person.cpf}>
+              <td>{person.name}</td>
+              <td>{person.age}</td>
+              <td>{person.civilStatus}</td>
+              <td>{person.cpf}</td>
+              <td>{person.city}</td>
+              <td>{person.state}</td>
+              <td style={{ maxWidth: '70px' }}>
+                <Button style={{ margin: '0 5px' }} onClick={() => handleShow(person)}><FaEdit /></Button>
+
+                <Button variant="danger" style={{ margin: '0 5px' }} onClick={() => onDeletePerson(person.cpf)}><FaTrashAlt /></Button>
+              </td>
+            </tr>
+          )) }
         </tbody>
       </Table>
+
+      <ModalUpdatePerson
+        showModal={show}
+        hideModal={handleClose}
+        onChangeName={(e) => setName(e.target.value)}
+        valueName={name}
+        onChangeAge={(e) => setAge(e.target.value)}
+        valueAge={age}
+        onChangeCivilStatus={(e) => setCivilStatus(e.target.value)}
+        valueCivilStatus={civilStatus}
+        onChangeCpf={(e) => setCpf(e.target.value)}
+        valueCpf={cpf}
+        onChangeCity={(e) => setCity(e.target.value)}
+        valueCity={city}
+        onChangeState={(e) => setState(e.target.value)}
+        valueState={state}
+        closeModal={handleClose}
+        onUpdatePerson={updatePerson}
+      />
     </Container>
   );
 }
---
+++ frontend/src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Reducers from '../reducers/reducer';
+
+
+const store = createStore(
+  Reducers,
+  applyMiddleware(thunk),
+);
+
+export default store;
